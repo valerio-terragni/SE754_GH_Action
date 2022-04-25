faa92517032aa92a147818a9f9869cc11f049fc4
valerio-terragni <66171778+valerio-terragni@users.noreply.github.com>
2022-04-25 22:03:18 +0800

    Update DollarTest.java

--- src/test/java/example/dollar/DollarTest.java
+++ src/test/java/example/dollar/DollarTest.java
@@ -6,9 +6,5 @@ import org.mockito.Mockito;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DollarTest {
-    @Test
-    public void testDollar(){
-        Dollar mockedDollar = Mockito.mock(Dollar.class);
-        assertEquals(6, mockedDollar.getAmount());
-    }
+
 }
